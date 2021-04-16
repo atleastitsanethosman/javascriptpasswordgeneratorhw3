5e71c12c5f037f8e495770ddd850ac023407581e
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-04-16 14:45:11 -0400

    refactored javascript for organization and better commenting, added information to readme.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # javascriptpasswordgeneratorhw3
-Javascript Password Generator HW 3 MSU Bootcamp
+## Javascript Password Generator HW 3 MSU Bootcamp
+
+## Michigan State University Coding Bootcamp<br>
+
+Project By: Ian Hennessey<br>
+GitHub Profile:  <https://github.com/atleastitsanethosman><br>
+Project GitHub Repository: <https://github.com/atleastitsanethosman/javascriptpasswordgeneratorhw3><br>
+Project URL: <https://atleastitsanethosman.github.io/javascriptpasswordgeneratorhw3/><br>
+
+## Project Description
+The purpose of this project was to use javascript to create a password generator that allows a user to request a password between 8 and 128 characters in length, and gives the user options to allow uppercase letters, lowercase letters, special characters, and numbers in the password.  Based on the allowed categories there is a function built in to generate a random string and populate it in the password window.  A GIF of the application function is displayed below.
+
+## GIF of Deployed Application
+![GIF of user pushing button to request password walking through prompts and receving password.](./assets/images/passwordgenerator.gif )
+
+## Project Elements:<br>
+
+1. The webpage has a button to press to generate a password.
+
+2. There is a prompt that comes up asking for a length between 8 and 128 characters, user will be alerted if not entering an acceptable value and function will end.
+
+3. There are a series of confirmation boxes that pop up asking the user about what will be allowed in the population of characters that will be randomly selected to make the password.  There are prompts for uppercase letters, lowercase letters, special characters, and numbers.
+
+4. If no characters are allowed an error message is returned indicating that at least one type must be selected and ends the function.
+
+5. If satisfactory inputs as noted above are given, the newly generated password displays in the webpage in a box for the user to see.
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// confirmation prompt variables for what is allowed to use in password and strings containing the data allowed by these prompts.
-var numeric;
-var numericlist = "0123456789";
-var characters;
-var characterslist = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var upperLetters;
-var upperLetterslist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerLetters;
-var lowerLetterslist = "abcdefghijklmnopqrstuvwxyz";
-var pwstring;
-var pwChoice = "";
-
 // function to create password, uses global variables for confirmation and strings above.
 function generatePassword() {
-  // prompt for password length and check to ensure it meets length criteria.
+
+  // prompt for password length and check to ensure it meets length criteria alerts errors if not acceptable.
   var len = parseInt(prompt("How many characters should your password be? (min of 8, max of 128"));
     if ( isNaN(len) ) {
       alert("must be a number, min of 8 and max of 128");
@@ -27,55 +16,62 @@ function generatePassword() {
         var characters = confirm("allow special characters?");
         var upperLetters = confirm("allow Uppercase letters?");
         var lowerLetters = confirm("allow Lowercase letters?");
-        // if and else if statements with functions to create a string based on allowed types from confirm prompts. 
-      } if (numeric == false && characters == false && upperLetters == false && lowerLetters == false) {
-        alert("you must select okay for at least one criteria");
-      } else if (numeric == true && characters == false && upperLetters == false && lowerLetters == false) {
-          pwstring = numericlist;
-      } else if (numeric == true && characters == true && upperLetters == false && lowerLetters == false) {
-          pwstring = numericlist.concat(characterslist);
-      } else if (numeric == true && characters == true && upperLetters == true && lowerLetters == false) { 
-          pwstring = numericlist.concat(characterslist + upperLetterslist);
-      } else if (numeric == true && characters == true && upperLetters == true && lowerLetters == true) {
-          pwstring = numericlist.concat(characterslist + upperLetterslist + lowerLetterslist);
-      } else if (numeric == false && characters == true && upperLetters == true && lowerLetters == true) {
-          pwstring = characterslist.concat(upperLetterslist + lowerLetterslist);
-      } else if (numeric == false && characters == false && upperLetters == true && lowerLetters == true) {
-          pwstring = upperLetterslist.concat(lowerLetterslist);
-      } else if (numeric == false && characters == false && upperLetters == false && lowerLetters == true) { 
-          pwstring = lowerLetterslist;
-      } else if (numeric == true && characters == true && upperLetters == false && lowerLetters == true) {
-          pwstring = numericlist.concat(characterslist + lowerLetterslist);
-      } else if (numeric == true && characters == false && upperLetters == true && lowerLetters == true) {
-          pwstring = numericlist.concat(upperLetterslist + lowerLetterslist);
-      } else if (numeric == true && characters == false && upperLetters == false && lowerLetters == true) {
-          pwstring = numericlist.concat(lowerLetterslist);
-      } else if (numeric == true && characters == false && upperLetters == true && lowerLetters == false) {
-        pwstring = numericlist.concat(upperLetterslist);
-      } else if (numeric == false && characters == true && upperLetters == true && lowerLetters == false) {
-        pwstring = characterslist.concat(upperLetterslist);
-      } else if (numeric == false && characters == true && upperLetters == false && lowerLetters == false) {
-        pwstring = characterslist
-      } else if (numeric == false && characters == true && upperLetters == false && lowerLetters == true) {
-        pwstring = characterslist.concat(lowerLetterslist)
-      } else if (numeric == false && characters == false && upperLetters == true && lowerLetters == false) {
-        pwstring = upperLetterslist;
-      }
 
-      // function to create a new string of randomly selected characters from the string created based on allowed character sets.
+        // variable to hold a string combining the different sets of characters allowed.
+        var pwstring;
+
+        // variables for strings containing the different types of characters allowed based on response to prompts.
+        var numericlist = "0123456789";
+        var characterslist = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        var upperLetterslist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var lowerLetterslist = "abcdefghijklmnopqrstuvwxyz";
+
+        // if and else if statements with functions to create a string based on allowed types from confirm prompts. 
+        } if (numeric == false && characters == false && upperLetters == false && lowerLetters == false) {
+          alert("you must select okay for at least one criteria");
+        } else if (numeric == true && characters == false && upperLetters == false && lowerLetters == false) {
+            pwstring = numericlist;
+        } else if (numeric == true && characters == true && upperLetters == false && lowerLetters == false) {
+            pwstring = numericlist.concat(characterslist);
+        } else if (numeric == true && characters == true && upperLetters == true && lowerLetters == false) { 
+            pwstring = numericlist.concat(characterslist + upperLetterslist);
+        } else if (numeric == true && characters == true && upperLetters == true && lowerLetters == true) {
+            pwstring = numericlist.concat(characterslist + upperLetterslist + lowerLetterslist);
+        } else if (numeric == false && characters == true && upperLetters == true && lowerLetters == true) {
+            pwstring = characterslist.concat(upperLetterslist + lowerLetterslist);
+        } else if (numeric == false && characters == false && upperLetters == true && lowerLetters == true) {
+            pwstring = upperLetterslist.concat(lowerLetterslist);
+        } else if (numeric == false && characters == false && upperLetters == false && lowerLetters == true) { 
+            pwstring = lowerLetterslist;
+        } else if (numeric == true && characters == true && upperLetters == false && lowerLetters == true) {
+            pwstring = numericlist.concat(characterslist + lowerLetterslist);
+        } else if (numeric == true && characters == false && upperLetters == true && lowerLetters == true) {
+            pwstring = numericlist.concat(upperLetterslist + lowerLetterslist);
+        } else if (numeric == true && characters == false && upperLetters == false && lowerLetters == true) {
+            pwstring = numericlist.concat(lowerLetterslist);
+        } else if (numeric == true && characters == false && upperLetters == true && lowerLetters == false) {
+          pwstring = numericlist.concat(upperLetterslist);
+        } else if (numeric == false && characters == true && upperLetters == true && lowerLetters == false) {
+          pwstring = characterslist.concat(upperLetterslist);
+        } else if (numeric == false && characters == true && upperLetters == false && lowerLetters == false) {
+          pwstring = characterslist
+        } else if (numeric == false && characters == true && upperLetters == false && lowerLetters == true) {
+          pwstring = characterslist.concat(lowerLetterslist)
+        } else if (numeric == false && characters == false && upperLetters == true && lowerLetters == false) {
+          pwstring = upperLetterslist;
+        }
+
+
+      // loop to create a new string of randomly selected characters from the string created based on allowed character sets.
+      // pwChoice used to hold the string created from the random selections..
+      var pwChoice = "";
       for ( i = 0;  i < len; i++)  {
         pwChoice += pwstring.charAt(Math.floor(Math.random() * pwstring.length));
-
       }
-  //returns random string to be used as password. 
+
+  //returns random selection string to be used as password. 
   return pwChoice
-
-      
-
-
 }
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -83,7 +79,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
